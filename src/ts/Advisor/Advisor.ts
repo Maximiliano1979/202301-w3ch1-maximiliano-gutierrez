906bb64bb06db9bee6210faf254bbf28f246bf64
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-01-24 18:31:03 +0100

    Update Characters

---
+++ src/ts/Advisor/Advisor.ts
@@ -0,0 +1,21 @@
+import { Character } from "../Character/Character";
+import {
+  type AdvisorStructre,
+  type CharacterDataStructure,
+} from "../types/types";
+
+export class Advisor extends Character implements AdvisorStructre {
+  constructor(
+    characterData: CharacterDataStructure,
+    public advises: Character
+  ) {
+    super(characterData);
+    if (advises instanceof Character) {
+      this.advises = advises;
+    }
+  }
+
+  communicate(): string {
+    return `${super.communicate()}I have a funny feeling that I'm going to die`;
+  }
+}
---
+++ src/ts/charcters.ts
@@ -0,0 +1,3 @@
+import { jaime, joffrey, bronn, daenerys, tyrion } from "./index";
+
+export const characters = [jaime, joffrey, daenerys, tyrion, bronn];
